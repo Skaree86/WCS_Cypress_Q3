42e2e30a9226465e5456862cebe1e8a9a0983f1a
Skaree86 <126167139+Skaree86@users.noreply.github.com>
2023-05-25 11:42:04 +0200

    quete cypress 4, voir fixture pour screenshot

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,11 @@
 const { defineConfig } = require("cypress");
+const { downloadFile } = require("cypress-downloadfile/lib/addPlugin");
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-    //  baseUrl: 'https://practice.expandtesting.com/notes/api'
+      //  baseUrl: 'https://practice.expandtesting.com/notes/api'
+      on("task", { downloadFile });
     },
   },
 });
